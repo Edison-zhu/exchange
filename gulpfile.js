69809a733f915d4c4dacc7a83a954becc5d86937
zyb955140 <384815317@qq.com>
2019-05-25 19:54:08 +0800

    修复更多bug

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,12 @@
-'use strict';
-
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var nodemon = require('gulp-nodemon');
-
-
 gulp.task('default', ['browser-sync'], function () {
 });
 
 gulp.task('browser-sync', ['nodemon'], function() {
     browserSync.init(null, {
-        proxy: "http://localhost:3000", // 注意这里要换成你在koa中设定的 服务端口一般是3000
+        proxy: "192.168.1.101:3000", // 注意这里要换成你在koa中设定的 服务端口一般是3000
         files: ["public/**/*.*","views","controller"],
         browser: "google chrome",
         port: 4000,
